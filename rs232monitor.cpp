d849b42b841d4af55a1fb3eb7a91b538feef4b67
Cedric de Wijs <cedric.dewijs@telfort.nl>
2018-04-14 07:33:39 +0200

    widgets now correctly nest

--- rs232monitor.cpp
+++ rs232monitor.cpp
@@ -4,29 +4,45 @@
 
 RS232MONITOR::RS232MONITOR(QWidget *parent) : QWidget(parent)
 {
+
+
+
+    myLog = new QTextBrowser();
+    myHlayout = new QSplitter();
+    myVlayout = new QSplitter();
+    rs232Rx = new RS232(myHlayout);
+    rs232Tx = new RS232(myHlayout);
+    myVlayout->setOrientation(Qt::Vertical);
+    myHlayout->setOrientation(Qt::Horizontal);
+    myHlayout->addWidget(rs232Rx);
+    myHlayout->addWidget(rs232Tx);
+    myVlayout->addWidget(myLog);
+    myVlayout->addWidget(myHlayout);
+    myVlayout->show();
+    /*
     qDebug()<<Q_FUNC_INFO;
     rs232Rx = new RS232(this);
     rs232Tx = new RS232(this);
     myLog = new QTextBrowser();
-    myHlayout = new QSplitter();
-    myVlayout = new QSplitter();
+    myHlayout = new QSplitter(this);
+    myVlayout = new QSplitter(this);
 
 
-//    myHlayout->setOrientation(Qt::Horizontal);
-//    myHlayout->addWidget(rs232Tx);
-//    myHlayout->addWidget(rs232Rx);
+    myHlayout->setOrientation(Qt::Horizontal);
+    myHlayout->addWidget(rs232Tx);
+    myHlayout->addWidget(rs232Rx);
 
 
     myVlayout->setOrientation(Qt::Vertical);
     myVlayout->addWidget(myLog);
-    myVlayout->addWidget(rs232Rx);
+    myVlayout->addWidget(myHlayout);
 
     rs232Rx->slotConnect(true);
     rs232Tx->slotConnect(true);
     myHlayout->setParent(parent);
     myVlayout->show();
-    rs232Rx->show();
-    rs232Tx->show();
+//    rs232Rx->show();
+//    rs232Tx->show();*/
 }
 
 RS232MONITOR::slotParseRx(QString msg)
